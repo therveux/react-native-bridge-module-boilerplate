2e88c0ccd3c8707c15eaca24458cc5fee804ab4b
Théo HERVEUX <hurobaki@gmail.com>
2019-05-11 13:48:02 +0200

    :boom: Export native module methods

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import { NativeModules } from 'react-native'
 
+const { RNBridgeModule } = NativeModules
+
 console.log(NativeModules);
+
+export const BridgeModule = {
+    usePromise: async () => {
+        const result = await RNBridgeModule.promiseRN()
+        console.log(`Promise result is : ${result}`);
+    },
+    useCallback: () => RNBridgeModule.callbackRN((err, result) => {
+        if (err) throw err
+        console.log(`Callback result is : ${result}`)
+    })
+}
