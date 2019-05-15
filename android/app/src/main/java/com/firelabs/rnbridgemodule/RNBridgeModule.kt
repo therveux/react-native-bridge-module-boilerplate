be45a91d27d2da4d0edf9391ea98e57641be30a3
Théo HERVEUX <hurobaki@gmail.com>
2019-05-15 11:16:35 +0200

    :wrench: delete android files

--- android/app/src/main/java/com/firelabs/rnbridgemodule/RNBridgeModule.kt
+++ android/app/src/main/java/com/firelabs/rnbridgemodule/RNBridgeModule.kt
@@ -1,18 +1,11 @@
 package com.firelabs.rnbridgemodule
 
 import com.facebook.react.bridge.ReactApplicationContext
-import com.facebook.react.bridge.ReactContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.WritableMap
 import com.facebook.react.modules.core.DeviceEventManagerModule
 
-class RNBridgeModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
-
-    private var react:ReactContext? = null
-
-    init {
-        react = reactContext
-    }
+class RNBridgeModule(private val context: ReactApplicationContext) : ReactContextBaseJavaModule(context) {
 
     companion object {
         const val MODULE_NAME = "RNBridgeModule"
@@ -20,10 +13,8 @@ class RNBridgeModule(reactContext: ReactApplicationContext) : ReactContextBaseJa
 
     private fun emitDeviceEvent(eventName: String, eventData: WritableMap?) {
         // https://facebook.github.io/react-native/docs/native-modules-android.html#sending-events-to-javascript
-        react!!.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter::class.java).emit(eventName, eventData)
+        context.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter::class.java).emit(eventName, eventData)
     }
-
-
 
     override fun getName() : String = MODULE_NAME
 }
